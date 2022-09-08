89ea42584197e98bbb11037ca52163fd20d52f13
Kiryl Pashkin <sleembelbox@gmail.com>
2022-09-08 23:11:36 +0300

    modified hanoy tower

--- src/Classes/HanoyTower.java
+++ src/Classes/HanoyTower.java
@@ -1,29 +1,48 @@
 package Classes;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class HanoyTower {
     private final ArrayList<String> movesArray;
+    private final int pancakes;
 
-    public HanoyTower(){
+    public HanoyTower(String firstPinName, String secondPinName, String thirdPinName) {
         this.movesArray = new ArrayList<String>();
+        this.pancakes = getPancakes();
+        calcTower(this.pancakes, firstPinName, secondPinName, thirdPinName);
     }
 
-    public void calcTower(int moves, String firstPin,String secondPin,String thirdPin) {
-        if(moves==1) {
+    private void calcTower(int moves, String firstPin, String secondPin, String thirdPin) {
+        if (moves == 1) {
             movesArray.add("Блинчик " + moves + " со шпильки " + firstPin + " на шпильку " + thirdPin + "");
             return;
         }
 
-        calcTower(moves-1, firstPin,thirdPin,secondPin);
+        calcTower(moves - 1, firstPin, thirdPin, secondPin);
         this.movesArray.add("Блинчик " + moves + " со шпильки " + firstPin + " на шпильку " + thirdPin + "");
-        calcTower(moves-1,secondPin,firstPin,thirdPin);
+        calcTower(moves - 1, secondPin, firstPin, thirdPin);
     }
 
-    public ArrayList<String> getResult(){
-        return this.movesArray;
-    }
-    public static void main(String[] args) {
+    public void printResult() {
+        Iterator<String> iterator = movesArray.iterator();
 
+        System.out.printf("\nДля количества блинчиков в башне %d решение следующее: \n", this.pancakes);
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    private int getPancakes() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Введите количество блинчиков в башне: ");
+        try {
+            return Integer.parseInt(reader.readLine());
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return -1;
+        }
     }
 }
--- src/HanoyTowerMain.java
+++ src/HanoyTowerMain.java
@@ -1,25 +1,9 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Iterator;
-
 import Classes.HanoyTower;
 
 public class HanoyTowerMain {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Введите количество блинчиков в башне: ");
-        int pancake = Integer.parseInt(reader.readLine());
-
-        HanoyTower tower = new HanoyTower();
-        tower.calcTower(pancake,"1", "2", "3");
-        ArrayList<String> movesArray = tower.getResult();
-        Iterator<String> iterator = movesArray.iterator();
-
-        System.out.printf("\nДля количества блинчиков в башне %d решение следующее: \n", pancake);
-        while (iterator.hasNext()){
-            System.out.println(iterator.next());
-        }
+        HanoyTower tower = new HanoyTower("1", "2", "3");
+        tower.printResult();
     }
 }
